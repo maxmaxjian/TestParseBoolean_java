0612fb152208d104c7ed720b3b42b3222d05f521
Jian Wei <wejian@amazon.com>
2019-08-14 19:01:27 -0400

    first submission

---
+++ src/main/java/Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Boolean bool = Boolean.parseBoolean("tRue");
+        System.out.println("bool = " + bool);
+    }
+}
